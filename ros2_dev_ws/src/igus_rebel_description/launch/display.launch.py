07bb7b05e347aa872e386bf3d8befffd535f37d0
Robin Heitz <blk_rh@wzl.rwth-aachen.de>
2023-02-17 20:36:15 +0000

    First working igus rebel description WS :))

--- ros2_dev_ws/src/igus_rebel_description/launch/display.launch.py
+++ ros2_dev_ws/src/igus_rebel_description/launch/display.launch.py
@@ -1,11 +1,14 @@
 import launch
 from launch.substitutions import Command, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 import launch_ros
 import os
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='igus_rebel_description').find('igus_rebel_description')
-    default_model_path = os.path.join(pkg_share, 'urdf/rebel.urdf')
+    default_model_path = os.path.join(pkg_share, 'urdf/igus_rebel.urdf')
+
+    model_arg = launch.actions.DeclareLaunchArgument(name="model", default_value=default_model_path, description="Absolute path to the robot urdf file.")
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
@@ -25,14 +28,8 @@ def generate_launch_description():
         name='foxglove_bridge',
     )
 
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-    )
-    
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot urdf file'),
+        model_arg,
         joint_state_publisher_node,
         robot_state_publisher_node,
         foxglove_node,
--- ros2_dev_ws/src/rebel_test/launch/display.launch.py
+++ ros2_dev_ws/src/rebel_test/launch/display.launch.py
@@ -7,6 +7,9 @@ def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='rebel_test').find('rebel_test')
     default_model_path = os.path.join(pkg_share, 'urdf/rebel.urdf')
 
+    model_arg = launch.actions.DeclareLaunchArgument(name="model", default_value=default_model_path, description="Absolute path to the robot urdf file.")
+    launch_foxglove = launch.actions.DeclareLaunchArgument(name="foxglove", default_value=True, description="Launch foxglove node for 3D-View (alternative to RViz2)")
+
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -26,8 +29,8 @@ def generate_launch_description():
     )
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot urdf file'),
-        # joint_state_publisher_node,
+        model_arg,
+        joint_state_publisher_node,
         robot_state_publisher_node,
-        # foxglove_node,
+        foxglove_node,
     ])
